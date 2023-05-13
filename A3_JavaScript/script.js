8c2e5b4ade653874a5f673fe945928094441d25b
sgarciamiras <129880649+sgarciamiras@users.noreply.github.com>
2023-05-13 20:51:11 +0200

    Add files via upload

--- A3_JavaScript/script.js
+++ A3_JavaScript/script.js
@@ -29,7 +29,33 @@ function fetchAPI(url,info,fotoPokemon) {
     })
 }
 
+//extraer información del pokemon seleccionado
+function fetchPokemon1(url) {
+    fetch (url)
+    .then(x => {
+        console.dir(x);
+        return x.json();
+    })
+    .then(y => {
+        console.dir(y);
+        document.getElementById(nom).innerHTML = y.name;
+        document.getElementById(fotoF).setAttribute('src',y.sprites.front_default);
+        document.getElementById(fotoB).setAttribute('src',y.sprites.back_default);
+        document.getElementById(atac).innerHTML = y.stats[1].base_stat;
+        document.getElementById(defe).innerHTML = y.stats[2].base_stat;
+        //falta el tipus
+    })
+}
 
+/*
+Nom = Objecte.name
+Imatge frontal = Objecte.sprites.front_defau
+lmatge posterior = Objecte.sprites.back_default
+Atac = Objecte.stats[1].base_stat
+Defensa = Objecte.stats[2].base_stat
+Tipus (poden ser varis) = Objecte.types[x].type.name : s'hauran
+de mostrar tots els tipus
+*/
 
 
 
