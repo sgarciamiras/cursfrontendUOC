294e83207a1b2661a8232933de705b39afe485fc
sgarciamiras <129880649+sgarciamiras@users.noreply.github.com>
2023-05-14 19:20:43 +0200

    Add files via upload

--- A3_JavaScript/script.js
+++ A3_JavaScript/script.js
@@ -3,8 +3,10 @@ const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 function cambiaTema(ev){
     if(ev.target.checked){
         document.documentElement.setAttribute('tema', 'light');
+        localStorage.setItem('tema', 'light');
     } else {
         document.documentElement.setAttribute('tema', 'dark');
+        localStorage.setItem('tema', 'dark');
     }
 }
 colorSwitch.addEventListener('change', cambiaTema);
